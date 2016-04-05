4f1ba02933c861a80d71cdcc327d48747da7d8e1
Austin McKinley <austinmckinley0@gmail.com>
2016-04-05 14:51:25 -0600

    Some unknown work

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component }  from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return <input onChange={this.onInputChange} />;
+  }
+
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+}
+
+export default SearchBar;
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBTyQyoIVHc_aJ9KQUNd83PtxRgFuZD-PA'
 
 const App = () => {
-  return <div>Hi!</div>
+  return  (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
